ad5a6b7148b332506358c9daf82037b60968a1fb
Zpice <ihsanxcaddy009@gmail.com>
2024-11-21 19:09:04 +0800

    fix validator for edit profiles and change pass

--- controller/auth/validator.js
+++ controller/auth/validator.js
@@ -45,3 +45,42 @@ export const joiLogin = Joi.object({
         'string.min': 'Password should have at least 6 characters',
     }),
 });
+
+export const editPass = Joi.object({
+    password: Joi.string().min(6).required()
+    .messages({
+        'string.empty': 'Password is required',
+        'string.min': 'Password should have at least 6 characters'
+    }),
+})
+
+export const joiEdit = Joi.object({
+    full_name: Joi.string().min(5).required()
+    .pattern(/^[A-Za-z\s]+$/)
+    .messages({
+        'string.pattern.base': 'Please enter a valid name   ', 
+        'string.empty': 'Full name is required',
+        'string.min': 'Full name should have at least 5 characters'
+        
+        
+    }),
+    email: Joi.string().email().required()
+    .messages({
+        'string.empty': 'Email is required',
+        'string.email': 'Please enter a valid email address'
+    }),
+    username: Joi.string().min(3).max(50).required().messages({
+        'string.empty': 'Username is required',
+        'string.min': 'Username must have at least 3 characters',
+        'string.max': 'Username must not exceed 50 characters'
+    }),
+    age: Joi.number().integer().required()
+    .messages({
+        'number.base': 'Please enter a valid number for age',
+        'any.required': 'Please enter your age'
+    }) ,
+    gender: Joi.string().valid('Laki Laki','Perempuan').required()
+    .messages({
+        'any.required': 'Gender is required'
+    }) ,      
+});
