18c48d1eb83689ea0183e667dbae82e1ae60ff23
Zpice <ihsanxcaddy009@gmail.com>
2024-11-30 09:36:40 +0800

    add link to get notif

--- controller/consult/consult.js
+++ controller/consult/consult.js
@@ -1,6 +1,7 @@
-import { Consultation, User } from "../../models/UserModel.js";
+import { Consultation, User,UserNotif } from "../../models/UserModel.js";
 import axios from "axios";
 import dotenv from 'dotenv';
+import {nanoid} from 'nanoid';
 dotenv.config();
 const Model_URL = process.env.MODEL_URL;
 console.log(Model_URL);
@@ -15,8 +16,7 @@ export const fetchApi = async (req, res) => {
         const user_id = req.user.id;
         const { username } = req.user;
         const payload = { ...req.body };
-        console.log('Sending payload to Flask API:', payload);
-        // Panggil Flask API
+     
 
         for (let key in payload){
             if (payload[key] ===null || payload[key] === undefined || payload[key] === ""){
@@ -28,21 +28,26 @@ export const fetchApi = async (req, res) => {
             Model_URL,
             payload, );
         
-        console.log('Flask API Response:', flaskResponse.data);
-
-        // Proses predictions dengan mengganti angka menjadi teks
         const processedPredictions = Object.fromEntries(
             Object.entries(flaskResponse.data.predictions).map(([key, value]) => [key, levelDescriptions[value] || "Tidak diketahui"])
         );
 
-        // Simpan ke database
-        await Consultation.create({
+        const consultation = await Consultation.create({
             user_id,
             predictions: processedPredictions,
             created_at: new Date(),
         });
+        const totalConsultations = await Consultation.count({ where: { user_id } });
+        const notificationMessage = `Selamat atas konsultasi ke-${totalConsultations} kamu.Yuk Cek di sini untuk melihat detailnya.`;
+        
+        await UserNotif.create({
+            user_id,
+            notif_id: nanoid(21),
+            notif_type: 'Konsultasi',
+            notif_content: notificationMessage,
+            history_id: consultation.id,  
+        });
 
-        // Kirim respons ke frontend
         res.status(200).json({
             user_id,
             username,
