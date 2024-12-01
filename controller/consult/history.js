1501e0e4c21f2dcda977b58e4cc0373ff17e1bb3
Zpice <ihsanxcaddy009@gmail.com>
2024-12-01 20:22:33 +0800

    fixed response history

--- controller/consult/history.js
+++ controller/consult/history.js
@@ -1,5 +1,5 @@
 import { Consultation } from "../../models/UserModel.js";
-
+import moment from 'moment-timezone';
 
 
 export const getHistory = async (req, res) => {
@@ -7,14 +7,22 @@ export const getHistory = async (req, res) => {
         
         const { id } = req.user; 
 
-       
-        const consultations = await Consultation.findAll({
+        
+       const consultations = await Consultation.findAll({
             where: { user_id: id }, 
             order: [['created_at', 'DESC']] 
         });
+        const Mapconsult = consultations.map(consultation => {
+            const createdAt = moment(consultation.created_at).format("YYYY-MM-DD HH:mm:ss");
+            return {
+                ...consultation.toJSON(),  
+                created_at: createdAt 
+            };
+        });
+
         res.status(200).json({
-            message: "History anda :",
-            history: consultations
+            message: "History anda:",
+            data: Mapconsult,  
         });
     } catch (error) {
         console.error("Error fetching history:", error.message);
